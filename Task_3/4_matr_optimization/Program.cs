0d7dcd4fa617e95fea6c6af35705676a6d7ee874
RussellMorryson <118932212+RussellMorryson@users.noreply.github.com>
2023-02-22 10:22:18 +0000

    Add loogic codes

---
+++ Task_3/4_matr_optimization/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+            matrix[i, j] = (i+1) * (j+1);
+            matrix[j, i] = (i+1) * (j+1);
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
