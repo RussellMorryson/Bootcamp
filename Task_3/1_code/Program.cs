ac6548d8f034d740c80ff63994a723d129243686
RussellMorryson <118932212+RussellMorryson@users.noreply.github.com>
2023-02-22 09:09:03 +0000

    Add codes

---
+++ Task_3/1_code/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 5;
+int [] array = new int [n]; //О(n)
+for (int i = 0; i < array.Length; i++) 
+    array[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+//Сложность алгоритма О(1)
+
+
+
+//Какое количество действий или операций до получения результата
---
+++ Task_3/3_num_table/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Упрощенный вариант представления таблицы умножения
+без формирвоания массива*/
+
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= n; j++)
+    {
+        Console.Write(i*j+"\t");
+    }
+    Console.WriteLine();
+}
